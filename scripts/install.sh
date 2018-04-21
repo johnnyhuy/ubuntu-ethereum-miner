cda159c68199ee5ef055c8bfb06aea603b5de29f
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-04-22 00:31:29 +1000

    Added overclock template.

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 # Config
 CLAYMORE_MINER_GZIP='claymore_11.6._quickfix'
 CLAYMORE_DIR='~/claymore'
-OVERCLOCK_START_SCRIPT='~/overclock.sh'
+OVERCLOCK_START_SCRIPT='$OVERCLOCK_START_SCRIPT'
 MINER_INSTALLER_DIR='~/miner-installer'
 MINER_START_SCRIPT='~/miner.sh'
 MINER_COOLDOWN=15
@@ -47,6 +47,37 @@ apt-get install nvidia-390
 
 echo -e "${YELLOW}\nUnlocking Nvidia overclocking setting"
 nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
+
+echo -e "\nCopying overclock template to ${OVERCLOCK_START_SCRIPT}"
+touch $OVERCLOCK_START_SCRIPT
+$OVERCLOCK_START_SCRIPT << __EOF__
+#!/bin/bash
+
+export DISPLAY=0
+export XAUTHORITY=/var/run/lightdm/root/:0
+
+# Memory clock
+# This setting is optional if you want to keep all your overclock settings the same
+MEMORY_OFFSET="300"
+
+# Enable persistent on device
+# To add another GPU, append the ID with a common on the same line
+# nvidia-smi -pm [XORG DEVICE # (e.g. 1,2,3)]
+nvidia-smi -pm 1
+
+# Power limit
+# To add another GPU, append the ID with a common on the same line
+# nvidia-smi -i [XORG DEVICE # (e.g. 1,2,3)] [POWER LIMIT (watt != percent)]
+nvidia-smi -i 0 -pl 80
+
+# Apply overclocking settings to each GPU
+# To add another GPU, duplicate the two active lines and set the Xorg device ID accordingly
+# nvidia-settings -a [gpu:[XORG DEVICE # (e.g. 1,2,3)]]/GpuPowerMizerMode=1
+# nvidia-settings -a [gpu:[XORG DEVICE # (e.g. 1,2,3)]]/GPUMemoryTransferRateOffset[2]=$MEMORY_OFFSET
+nvidia-settings -a [gpu:0]/GpuPowerMizerMode=1
+nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffset[2]=$MEMORY_OFFSET
+
+__EOF__
 
 echo -e "${YELLOW}\nInstalling Claymore Miner to ${CLAYMORE_DIR}"
 mkdir "${MINER_INSTALLER_DIR}/claymore_extract"
