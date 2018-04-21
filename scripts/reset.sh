f135dece2ca49d6b3cee4390bd6f7bdcf169218b
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-04-22 08:47:49 +1000

    Added reset script.

---
+++ scripts/reset.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Config
+WELCOME_MESSAGE="${CYAN}Welcome to the reset script${RESET}"
+
+# Colors
+RESET='\033[0m'
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+
+if [[ $EUID > 0 ]]; then
+    echo -e $WELCOME_MESSAGE
+    echo -e "${RED}Permission denied, please run this script in root/sudo${RESET}"
+    exit 1
+fi
+
+echo -e $WELCOME_MESSAGE
+echo -e "${YELLOW}Running this script in root/sudo${RESET}"
+
+echo -e "${GREEN}\nThis reset script will remove the following:
+    \n/etc/modprobe.d/blacklist-nouveau.conf
+    \n~/claymore/
+    \n~/miner.sh
+    \n~/overclock.sh${YELLOW}"
+read -e -n 1 -r -p "Confirm [y/N] " INPUT
+case $INPUT in
+    [yY])
+    break
+    ;;
+    [nN]|"")
+    echo -e "${RED}Installation aborted!${RESET}"
+    exit 1
+    ;;
+    *)
+    echo -e "${RED}Please choose y or n.${RESET}"
+    ;;
+esac
+
+rm -rdf /etc/modprobe.d/blacklist-nouveau.conf ~/claymore/ ~/miner.sh ~/overclock.sh
