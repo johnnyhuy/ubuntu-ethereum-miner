074f7f8bf9b0324afe46b045e834ce7bf0ec3ea9
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-04-22 10:45:39 +1000

    Added username prompt and cleaned up echo format.

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,10 +13,6 @@ WHITE='\033[0;37m'
 
 # Config
 CLAYMORE_MINER_GZIP='claymore_11.6_quickfix'
-CLAYMORE_DIR=./claymore
-OVERCLOCK_START_SCRIPT=./overclock.sh
-MINER_INSTALLER_DIR=./miner-installer
-MINER_START_SCRIPT=./miner.sh
 MINER_COOLDOWN=15
 OVERCLOCK_COOLDOWN=30
 WELCOME_MESSAGE="${CYAN}Welcome to the johnnyhuy/ubuntu-etheruem-miner installer${RESET}"
@@ -28,17 +24,41 @@ if [[ $EUID > 0 ]]; then
 fi
 
 echo -e $WELCOME_MESSAGE
-echo -e "${YELLOW}Running this script in root/sudo${RESET}"
+echo -e "${YELLOW}Running this script in root/sudo\n${RESET}"
+
+read -e -p "Enter your username: " USERNAME
+while :
+do
+    read -e -n 1 -r -p "Confirm username is: ${USERNAME} [y/N] " INPUT
+    case $INPUT in
+        [yY])
+        break
+        ;;
+        [nN]|"")
+        echo -e "${RED}Installation aborted!${RESET}"
+        exit 1
+        ;;
+        *)
+        echo -e "${RED}Please choose y or n.${RESET}"
+        ;;
+    esac
+done
+
+# Directories
+CLAYMORE_DIR=/home/$USERNAME/claymore
+OVERCLOCK_START_SCRIPT=/home/$USERNAME/overclock.sh
+MINER_INSTALLER_DIR=/home/$USERNAME/miner-installer
+MINER_START_SCRIPT=/home/$USERNAME/miner.sh
 
 echo -e "${GREEN}\nThis installation script will do the following:
-1. Install Ubuntu utility packages: git vim screen openssh-server
-2. Install Nvidia drivers: installs package nvidia-390
-3. Unlock Nvidia overclock settings: runs nvidia-xconfig
-4. Extract Claymore's Miner from ./miner-installer/miner and install at ~/claymore
-5. Disable nouveau: adds config at /etc/modprobe.d/blacklist-nouveau.conf
-6. Create overclock script: added script at ./overclock.sh
-7. Create miner start script: added script at ./miner.sh
-8. Create cron job: starts miner and overclock at reboot\n${RESET}"
+${CYAN}1) ${YELLOW}Install Ubuntu utility packages: ${RESET}git vim screen openssh-server
+${CYAN}2) ${YELLOW}Install Nvidia drivers: ${RESET}installs package nvidia-390
+${CYAN}3) ${YELLOW}Unlock Nvidia overclock settings: ${RESET}runs nvidia-xconfig
+${CYAN}4) ${YELLOW}Extract Claymore's Miner from ./miner-installer/miner and install at ~/claymore
+${CYAN}5) ${YELLOW}Disable nouveau: ${RESET}adds config at /etc/modprobe.d/blacklist-nouveau.conf
+${CYAN}6) ${YELLOW}Create overclock script: ${RESET}added script at ./overclock.sh
+${CYAN}7) ${YELLOW}Create miner start script: ${RESET}added script at ./miner.sh
+${CYAN}8) ${YELLOW}Create cron job: ${RESET}starts miner and overclock at reboot\n"
 while :
 do
     read -e -n 1 -r -p "Confirm [y/N] " INPUT
@@ -88,5 +108,7 @@ echo -e "${CYAN}\n(8/8)${YELLOW} Creating crontab to start miner at boot${RESET}
 bash "${MINER_INSTALLER_DIR}/scripts/_create_cron_job.sh" $MINER_COOLDOWN $MINER_START_SCRIPT $OVERCLOCK_COOLDOWN $OVERCLOCK_START_SCRIPT
 
 echo -e "${GREEN}\nInstallation complete, restarting in 5 seconds (manual reboot if required)${RESET}"
+chown -R $USERNAME:$USERNAME $OVERCLOCK_START_SCRIPT
+chown -R $USERNAME:$USERNAME $MINER_START_SCRIPT
 
 @sleep 5 reboot
