7e30c836a058f6b727bb4f972b0471a0dda017b0
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-04-22 10:11:02 +1000

    Added more info on startup and progress.

--- scripts/install.sh
+++ scripts/install.sh
@@ -31,13 +31,14 @@ echo -e $WELCOME_MESSAGE
 echo -e "${YELLOW}Running this script in root/sudo${RESET}"
 
 echo -e "${GREEN}\nThis installation script will do the following:
-Install Ubuntu utility packages: git vim screen openssh-server
-Disable nouveau: adds config at /etc/modprobe.d/blacklist-nouveau.conf
-Install Nvidia drivers: installs package nvidia-390
-Unlock Nvidia overclock settings: runs nvidia-xconfig
-Create overclock script: added script at ./overclock.sh
-Create miner start script: added script at ./miner.sh
-Create cron job: starts miner and overclock at reboot\n${RESET}"
+1. Install Ubuntu utility packages: git vim screen openssh-server
+2. Install Nvidia drivers: installs package nvidia-390
+3. Unlock Nvidia overclock settings: runs nvidia-xconfig
+4. Extract Claymore's Miner from ./miner-installer/miner and install at ~/claymore
+5. Disable nouveau: adds config at /etc/modprobe.d/blacklist-nouveau.conf
+6. Create overclock script: added script at ./overclock.sh
+7. Create miner start script: added script at ./miner.sh
+8. Create cron job: starts miner and overclock at reboot\n${RESET}"
 while :
 do
     read -e -n 1 -r -p "Confirm [y/N] " INPUT
@@ -54,32 +55,35 @@ do
         ;;
     esac
 done
-echo -e "${YELLOW}\nUpdating/Upgrading Ubuntu packages${RESET}"
+echo -e "${YELLOW}Updating/Upgrading Ubuntu packages${RESET}"
 add-apt-repository ppa:graphics-drivers/ppa -y
 apt-get update
 # apt-get upgrade -y
 
-echo -e "${YELLOW}\nInstalling Ubuntu utilities (git, vim etc.)${RESET}"
+echo -e "${CYAN}\n(1/8)${YELLOW} Installing Ubuntu utilities (git, vim etc.)${RESET}"
 apt-get install git vim screen openssh-server -y
 
-echo -e "${YELLOW}\nInstalling Nvidia drivers${RESET}"
+echo -e "${CYAN}\n(2/8)${YELLOW} Installing Nvidia drivers${RESET}"
 apt-get install nvidia-390 -y
 
-echo -e "${YELLOW}\nDisabling nouveau${RESET}"
+echo -e "${CYAN}\n(3/8)${YELLOW} Unlocking Nvidia overclock setting${RESET}"
+nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
+
+echo -e "${CYAN}\n(4/8)${YELLOW} Disabling nouveau${RESET}"
 bash "${MINER_INSTALLER_DIR}/scripts/_disable_nouveau.sh"
 
-echo -e "${YELLOW}\nCopying overclock template to ${OVERCLOCK_START_SCRIPT}${RESET}"
+echo -e "${CYAN}\n(5/8)${YELLOW} Copying overclock template to ${OVERCLOCK_START_SCRIPT}${RESET}"
 bash "${MINER_INSTALLER_DIR}/scripts/_nvidia_overclock.sh" $OVERCLOCK_START_SCRIPT
 
-echo -e "${YELLOW}\nInstalling Claymore Miner to ${CLAYMORE_DIR}${RESET}"
+echo -e "${CYAN}\n(6/8)${YELLOW} Installing Claymore Miner to ${CLAYMORE_DIR}${RESET}"
 bash "${MINER_INSTALLER_DIR}/scripts/_create_miner.sh" $MINER_INSTALLER_DIR $CLAYMORE_MINER_GZIP $CLAYMORE_DIR
 
-echo -e "${YELLOW}\nCopying template miner start script${RESET}"
+echo -e "${CYAN}\n(7/8)${YELLOW} Copying template miner start script${RESET}"
 echo -e "${BLUE}Change to appropriate miner settings after you run this script${RESET}"
 touch $MINER_START_SCRIPT
 echo -e "#!/bin/bash\n${CLAYMORE_DIR}/ethdcrminer64 -epool [POOL] -ewal [ETH WALLET ADDR].[WORKER NAME]/[EMAIL] -epsw x -mode 1 -ftime 10" >> ${MINER_START_SCRIPT}
 
-echo -e "${YELLOW}\nCreating crontab to start miner at boot${RESET}"
+echo -e "${CYAN}\n(8/8)${YELLOW} Creating crontab to start miner at boot${RESET}"
 bash "${MINER_INSTALLER_DIR}/scripts/_create_cron_job.sh" $MINER_COOLDOWN $MINER_START_SCRIPT $OVERCLOCK_COOLDOWN $OVERCLOCK_START_SCRIPT
 
 echo -e "${GREEN}\nInstallation complete, restarting in 5 seconds (manual reboot if required)${RESET}"
