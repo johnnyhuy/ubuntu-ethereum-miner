c414bccb48b77c5b680808461aa0eca21c7320d9
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-04-22 13:57:53 +1000

    Do not use newline characters.

--- scripts/_nvidia_overclock.sh
+++ scripts/_nvidia_overclock.sh
@@ -16,24 +16,30 @@ WHITE='\033[0;37m'
 
 touch $OVERCLOCK_START_SCRIPT
 echo -e"#!/bin/bash
-\nif [[ \$EUID > 0 ]]; then
+
+if [[ \$EUID > 0 ]]; then
     echo -e \"\\033[0;31mPermission denied, please run this script in root/sudo\\033[0m\"
     exit 1
 fi
-\nexport DISPLAY=0
+
+export DISPLAY=0
 export XAUTHORITY=/var/run/lightdm/root/:0
-\n# Memory clock
+
+# Memory clock
 # This setting is optional if you want to keep all your overclock settings the same
 MEMORY_OFFSET=\"300\"
-\n# Enable persistent on device
+
+# Enable persistent on device
 # To add another GPU, append the ID with a common on the same line
 # nvidia-smi -pm [XORG DEVICE # (e.g. 1,2,3)]
 nvidia-smi -pm 1
-\n# Power limit
+
+# Power limit
 # To add another GPU, append the ID with a common on the same line
 # nvidia-smi -i [XORG DEVICE # (e.g. 1,2,3)] [POWER LIMIT (watt != percent)]
 nvidia-smi -i 0 -pl 80
-\n# Apply overclocking settings to each GPU
+
+# Apply overclocking settings to each GPU
 # To add another GPU, duplicate the two active lines and set the Xorg device ID accordingly
 # nvidia-settings -a [gpu:[XORG DEVICE # (e.g. 1,2,3)]]/GpuPowerMizerMode=1
 # nvidia-settings -a [gpu:[XORG DEVICE # (e.g. 1,2,3)]]/GPUMemoryTransferRateOffset[2]=$MEMORY_OFFSET
