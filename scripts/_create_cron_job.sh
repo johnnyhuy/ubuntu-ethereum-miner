ae820a879d9e026d363547e5bdb7fbe5547b0445
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-04-28 00:21:39 +1000

    Replaced reboot command.

--- scripts/_create_cron_job.sh
+++ scripts/_create_cron_job.sh
@@ -6,22 +6,11 @@ MINER_START_SCRIPT=$2
 OVERCLOCK_COOLDOWN=$3
 OVERCLOCK_START_SCRIPT=$4
 
-# Colors
-RESET='\033[0m'
-BLACK='\033[0;30m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-WHITE='\033[0;37m'
-
 crontab -l > ./temp_cron
 echo -e "@reboot nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
 @reboot sleep ${MINER_COOLDOWN} && screen -dmS claymore sh ${MINER_START_SCRIPT}
 @reboot sleep ${OVERCLOCK_COOLDOWN} && sh ${OVERCLOCK_START_SCRIPT}
 
-0 */1 * * * shutdown -r" > ./temp_cron
+0 */1 * * * reboot" > ./temp_cron
 crontab ./temp_cron
 rm ./temp_cron
