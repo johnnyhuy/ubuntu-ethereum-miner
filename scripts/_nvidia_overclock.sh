5b2e9f655d4e4bf1494d36811a464d9fab4ce5c0
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-04-28 00:21:59 +1000

    Removed unused vars.

--- scripts/_nvidia_overclock.sh
+++ scripts/_nvidia_overclock.sh
@@ -3,17 +3,6 @@
 # Config
 OVERCLOCK_START_SCRIPT=$1
 
-# Colors
-RESET='\033[0m'
-BLACK='\033[0;30m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-WHITE='\033[0;37m'
-
 touch $OVERCLOCK_START_SCRIPT
 echo -e "#!/bin/bash
 
