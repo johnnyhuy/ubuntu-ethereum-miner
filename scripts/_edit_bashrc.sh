ab42e8e08b7a8c3f582c4e929cf0b07116df6b61
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-04-28 00:38:44 +1000

    Revered condition to fix dupe edit.

--- scripts/_edit_bashrc.sh
+++ scripts/_edit_bashrc.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Colors
+RESET='\033[0m'
+RED='\033[0;31m'
+
 # Config
 USER_DIR=$1
 
 if grep -Fxq "export DISPLAY=:0" $USER_DIR/.bashrc
 then
+    echo -e "${RED}The .bachrc has already been modified.${RESET}"
+else
     echo -e "\nexport DISPLAY=:0\nxhost + SI:localuser:root" >> $USER_DIR/.bashrc
 fi
