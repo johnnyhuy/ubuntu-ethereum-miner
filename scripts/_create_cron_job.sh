b4484d8f141be6580e8e42fc7d6fe1473b13de63
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2018-04-29 09:57:09 +1000

    Renamed eth miner screen.

--- scripts/_create_cron_job.sh
+++ scripts/_create_cron_job.sh
@@ -7,7 +7,7 @@ OVERCLOCK_COOLDOWN=$3
 OVERCLOCK_START_SCRIPT=$4
 
 crontab -l > ./temp_cron
-echo -e "@reboot sleep ${MINER_COOLDOWN} && screen -dmS claymore bash ${MINER_START_SCRIPT}
+echo -e "@reboot sleep ${MINER_COOLDOWN} && screen -dmS ethminer bash ${MINER_START_SCRIPT}
 @reboot sleep ${OVERCLOCK_COOLDOWN} && bash ${OVERCLOCK_START_SCRIPT}
 
 0 */1 * * * reboot" > ./temp_cron
